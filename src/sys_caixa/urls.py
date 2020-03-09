fe962effbda54ec194946da15111816c7dede88b
wagner <wagnercl31@hotmail.com>
2020-03-09 18:26:14 -0300

    creat and set .gitignore

--- src/sys_caixa/urls.py
+++ src/sys_caixa/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home_page
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+        path('', home_page),
+        path('admin/', admin.site.urls),
 ]
