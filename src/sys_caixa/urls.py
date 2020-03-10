ad3495cd7a58be5fd2cf7356df243c6e409c026d
wagner <wagnercl31@hotmail.com>
2020-03-09 21:56:23 -0300

    created about_page and contact_page

--- src/sys_caixa/urls.py
+++ src/sys_caixa/urls.py
@@ -19,8 +19,8 @@ from .views import home_page, about_page, contact_page
 
 
 urlpatterns = [
-            path('', home_page),
-            path('about/', about_page),
-            path('contact/', contact_page),
-            path('admin/', admin.site.urls),
+        path('', home_page),
+        path('about/', about_page),
+        path('contact/', contact_page),
+        path('admin/', admin.site.urls),
 ]
